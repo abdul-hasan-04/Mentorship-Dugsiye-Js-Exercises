51946400066b1c8129684a1155b5b3b5dc8ce7a7
abdul-hasan <abdalamohamedhaazzan@gmail.com>
2026-02-06 13:35:28 +0300

    i added exercise 4, both html file and js file

---
+++ Exercise_4 js/main.js
@@ -0,0 +1,30 @@
+// function decelaration
+
+function add(num1,num2){
+    return num1 + num2
+}
+
+let product = add(2,2);
+console.log(product);
+product = add(5,5);
+console.log(product);
+product= add(9,9)
+console.log(product);
+product = add(34,70);
+console.log(product);
+
+
+// function expression//
+
+const iskudare = function(val1, val2){
+    return val1 + val2
+}
+
+let sum = iskudare(30,40);
+console.log(sum);
+sum = iskudare(5,5);
+console.log(sum);
+sum = iskudare (90,90);
+console.log(sum);
+
+
