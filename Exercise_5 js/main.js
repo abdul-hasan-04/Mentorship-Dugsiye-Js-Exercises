f77a4a3033401bf9b0a1ef645433c2a56e84a454
abdul-hasan <abdalamohamedhaazzan@gmail.com>
2026-02-06 20:22:01 +0300

    i added exercise 5 both it's html and main.js file

---
+++ Exercise_5 js/main.js
@@ -0,0 +1,22 @@
+// creating add arrow function that takes two parameters and then returns parameter 1 + parameter 2//
+
+const add = (a,b) =>{
+    return a + b;
+}
+
+let products = add (4,5);
+console.log(products);
+products = add (10,20);
+console.log(products);
+products = add (10,40);
+console.log(products);
+products = add(2,80);
+console.log(products);
+products = add(99, 1);
+console.log(products);
+
+
+
+
+
+
