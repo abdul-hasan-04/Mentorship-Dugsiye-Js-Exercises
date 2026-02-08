a8998e7ad883ce69cc9744d27698350319f70dbf
abdul-hasan <abdalamohamedhaazzan@gmail.com>
2026-02-08 14:32:38 +0300

    i added exercise 8 both it's html and main.js file

---
+++ Exercise_7 js/main.js
@@ -0,0 +1,13 @@
+let colors = ["Red", "Green", "Blue", "Black"];
+
+colors.push("Orange");
+
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+
+
+
+
---
+++ Exercise_8 js/main.js
@@ -0,0 +1,9 @@
+ let car = {
+    make : "Toyota",
+    model : "Harrier",
+    year : 2014
+}
+
+console.log(car.make);
+console.log(car["model"]);
+console.log(car.year);
