13dc8d7445f20797cdd5393275fcea2da4d03e63
abdul-hasan <abdalamohamedhaazzan@gmail.com>
2026-02-08 15:24:19 +0300

    i added exercise 10 of js

---
+++ Exercise_10 js/main.js
@@ -0,0 +1,25 @@
+const library = [
+
+    {
+        title : "Bird of happiness",
+        author : "Salim Polak",
+        year : 1989
+    },
+
+    {
+        title : "HousMaid",
+        author : "Mei Robbins",
+        year : 2015
+    }, 
+
+    {
+        title : "Unrivaled",
+        author : "james",
+        year : 2012
+    }
+
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
+
